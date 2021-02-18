5d65d45a64f278987119e5538526f7c8bcd81056
ellioday <elliott@dayabode.plus.com>
2021-02-18 17:06:57 +0000

    added magnetic coordinate calculations

--- pydatadarn/classes/station.py
+++ pydatadarn/classes/station.py
@@ -1,105 +1,123 @@
 import numpy as np
-
 from pydatadarn.utils import tools
 from pydatadarn.utils import coordinate_transformations as coords
+import os
+import datetime
+
+#get path to superdarn rst
+rst = os.environ["RSTPATH"]
+hdw_path = rst + "/tables/superdarn/hdw/"
+
+#generate arrays for the data
+#for more information see hdw tables in supderdarn rst
 
 class Station():
 	
-	
 	"""
-	creates a station containing data with coordinates in:
-		geographical (lat/lon)
-		aacgm (mlat/mlon)
+	A class used to access and store hardware data for superdarn radar stations. 
+	Data is collected from the superdarn rst (required) hdw tables.
 	"""
 	
-	def __init__(self, lat, lon, look):
+	def __init__(self, rad):
 		
 		"""
 		Parameters
 		----------
 		
-		lat: float
-			geographical latitude
-			
-		lon: float
-			geographical longitude
+		rad: string
+			3 letter station code for requested radar data (e.g. "ade")
 		"""
 		
-		self.lat = lat
-		self.lon = lon
-		self.look = look
+		#find hdw file
+		fname = "hdw.dat.{}".format(rad)
+		file = hdw_path + fname
 		
-		return
+		#generate arrays for the data
+		#for more information see hdw tables in supderdarn rst
+		self.stat_id = np.array([], dtype = "int")
+		self.years = np.array([], dtype = "int")
+		self.secs = np.array([], dtype = "int")
+		self.glats = np.array([], dtype = "float")
+		self.glons = np.array([], dtype = "float")
+		self.alts = np.array([], dtype = "float")
+		self.boresight = np.array([], dtype = "float")
+		self.beam_sep = np.array([], dtype = "float")
+		self.vel_sign = np.array([], dtype = "int")
+		self.an_rx_atten_db = np.array([], dtype = "int")
+		self.tdiff = np.array([], dtype = "float")
+		self.phase_sign = np.array([], dtype = "int")
+		self.interf_offset_x = np.array([], dtype = "float")
+		self.interf_offset_y = np.array([], dtype = "float")
+		self.interf_offset_z = np.array([], dtype = "float")
+		self.an_rx_rise_time = np.array([], dtype = "float")
+		self.an_atten_stages = np.array([], dtype = "int")
+		self.max_range_gates = np.array([], dtype = "int")
+		self.max_beams = np.array([], dtype = "int")
+		self.dtimes = np.array([])
+
+		#open the file		
+		with open(file) as fp:
+			lines = fp.readlines()
+			for line in lines[98:]:
+				if line[0] == "#":
+					continue
+				else:
+					#add data to arrays
+					data = line.split()
+					self.stat_id = np.append(self.stat_id, int(data[0]))
+					#if final entry for year is a ridiculously large number
+					#we can run into compatibility issues so set it to current
+					#date
+					year = int(data[1])
+					sec = int(data[2])
+					if year > datetime.datetime.now().year :
+						year = datetime.datetime.now().year
+						#also set the seconds to be the time now
+						sec = datetime.datetime.now().timetuple().tm_yday*24*3600
+					self.years = np.append(self.years, year)
+					self.secs = np.append(self.secs, sec)
+					self.glats = np.append(self.glats, float(data[3]))
+					self.glons = np.append(self.glons, float(data[4]))
+					self.alts = np.append(self.alts, float(data[5]))
+					self.boresight = np.append(self.boresight, float(data[6]))
+					self.beam_sep = np.append(self.beam_sep, float(data[7]))
+					self.vel_sign = np.append(self.vel_sign, float(data[8]))
+					self.an_rx_atten_db = np.append(self.an_rx_atten_db, int(data[9]))
+					self.tdiff = np.append(self.tdiff, float(data[10]))
+					self.phase_sign = np.append(self.phase_sign, int(data[11]))
+					self.interf_offset_x = np.append(self.interf_offset_x, float(data[12]))
+					self.interf_offset_y = np.append(self.interf_offset_y, float(data[13]))
+					self.interf_offset_z = np.append(self.interf_offset_z, float(data[14]))
+					self.an_rx_rise_time = np.append(self.an_rx_atten_db, float(data[15]))
+					self.an_atten_stages = np.append(self.an_atten_stages, int(data[16]))
+					self.max_range_gates = np.append(self.max_range_gates, int(data[17]))
+					self.max_beams = np.append(self.max_beams, int(data[18]))
+					#get datetimes
+					dtime = datetime.datetime(year, 1, 1, 0, 0, 0) + datetime.timedelta(seconds=int(data[2]))
+					self.dtimes = np.append(self.dtimes, dtime)
 		
-	def set_aacgm(self, time):
-		
+	def get_aacgm(self, dtime):			
+	
 		"""
-		gets aacgmv2 coordinates from geographical
+		Calculates and returns aacmgv2 coordinates for this radar from the
+		specified time
 		
 		Parameters
 		----------
 		
 		time: datetime object
-			datetime object of format datetime.datetime(YY, MM, DD, hh, mm, ss)
+			datetime object of format datetime.datetime(YYYY, MM, DD, hh, mm, ss)
 		"""
-		self.mlat, self.mlon = coords.geo_to_aacgm(self.lat, self.lon, time)
-		if isinstance(self.mlat, np.ndarray):
-			self.mlat = self.mlat[0]
-		if isinstance(self.mlon, np.ndarray):
-			self.mlon = self.mlon[0]
-		self.mcolat = 90 - self.mlat
-		
-		return
 	
-	def get_mlt(self, time):
+		#get first time in station metadata that is greater
+		#than time of data requested. The correct data will
+		#then be the metadata before that
+		station_time_index = min(np.where(self.dtimes > dtime)[0])-1
+		glat = self.glats[station_time_index]
+		glon = self.glons[station_time_index]	
+		alt = self.alts[station_time_index]
+
+		#get mlat and mlon
+		mlat, mlon = coords.geo_to_aacgm(glat, glon, dtime, alt)
 		
-		"""
-		gets magnetic local time from magnetic longitude
-		
-		Parameters
-		----------
-		time: datetime object
-			datetime object of format datetime.datetime(YY, MM, DD, hh, mm, ss)
-		"""
-		
-		mlt = coords.aacgm_to_mlt(self.mlon, time)
-		
-		if isinstance(mlt, np.ndarray):
-			mlt = mlt[0]
-		
-		return mlt
-		
-	def set_boresight(self, boresight):
-		
-		"""
-		Takes geographical boresight and calculates magnetic north boresight
-		
-		Parameters
-		----------
-		
-		boresight: float
-			angle of boresight (in radians)
-		time: dtime object
-			time of interest
-		"""
-		
-		self.boresight_geo = boresight
-		
-		return
-	
-	def set_beam_number(self, beam_number):
-		self.beam_number = beam_number
-		return
-	def set_beam_width(self, beam_width):
-		self.beam_width = beam_width
-		return
-	def set_range_gates(self, range_gates):
-		self.range_gates = range_gates
-	
-		#create polar field of view meshgrid.
-		beam_from = self.boresight-(0.5*self.beam_number*self.beam_width)
-		beam_to = self.boresight+(0.5*self.beam_number*self.beam_width)
-		self.beam_range = np.arange(beam_from, beam_to, self.beam_width)
-		self.gates_range = np.arange(0, self.range_gates+1, 1)
-		
-		return	
+		return mlat, mlon
